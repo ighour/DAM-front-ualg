53a93e0bf935668acaf9ba8c766e50ff1127324f
Ighour <ti.celio.rodrigues@gmail.com>
2018-04-13 11:58:38 +0100

    Created simple nav

--- src/components/partials/Menu.vue
+++ src/components/partials/Menu.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
-        MENU
-    </div>
+    <b-nav fill tabs>
+        <b-nav-item :to="{name: 'index'}" exact>Home</b-nav-item>
+        <b-nav-item :to="{name: 'movie'}">Movies</b-nav-item>
+        <b-nav-item :to="{name: 'genre'}">Genres</b-nav-item>
+        <b-nav-item disabled>Not Logged In</b-nav-item>
+    </b-nav>
 </template>

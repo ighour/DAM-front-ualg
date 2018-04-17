0421a1f3d8bb7ac5c3d90eb2ab08caee638c04a1
Ighour <ti.celio.rodrigues@gmail.com>
2018-04-17 20:22:01 +0100

    Configured nodejs express server for use in Heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+let express = require('express')
+let path = require('path')
+let serveStatic = require('serve-static')
+
+var app = express()
+
+app.use(serveStatic(__dirname + "/dist"))
+
+let port = process.env.PORT || 5000
+app.listen(port)
+
+console.log('Server started on port '+ port)
